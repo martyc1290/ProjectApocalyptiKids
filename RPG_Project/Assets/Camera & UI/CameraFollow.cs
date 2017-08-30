f586bc28544b05f9bd3d8ac19bb52f04b7312e23
Marty Cawley <martyc1290@yahoo.com>
2017-08-29 21:30:32 -0500

    Initial Udemy RPG Section 1 commit
    It appears to finally be committing all files to git...

---
+++ RPG_Project/Assets/Camera & UI/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position;
+	}
+}
